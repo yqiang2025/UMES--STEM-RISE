e8143b70bac2beb0ca9ecfa88f79c50a417cf12e
yqiang2025 <yqiang@umes.edu>
2026-05-05 14:23:07 -0400

    Update Google Sheets Web App URL

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Paste your deployed Google Apps Script Web App URL here.
       // Example: const GOOGLE_SHEETS_WEB_APP_URL = "https://script.google.com/a/macros/umes.edu/s/AKfycbxC0koJ8djUPcliZP0bmJ6LiBbskvSRyEOii-XKD_WfQ4S7lrod8iPFj2301X5fi9hg/exec";
-      const GOOGLE_SHEETS_WEB_APP_URL = "https://script.google.com/a/macros/umes.edu/s/AKfycbxC0koJ8djUPcliZP0bmJ6LiBbskvSRyEOii-XKD_WfQ4S7lrod8iPFj2301X5fi9hg/exec";
+      const GOOGLE_SHEETS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyGmLqMLpFhmBtTfmbiMpOTYBDBvp6FOF44pyf3qe_9OH9ek8XdisXQVFmSi3Tjcsnc/exec";
 
       const coordinatorEmail = "yqiang@umes.edu";
       const form = document.getElementById("registrationForm");
